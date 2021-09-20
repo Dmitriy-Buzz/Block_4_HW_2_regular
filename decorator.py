703cfd5828f9dff9f5816df58a1df037cb7ea0c0
Dmitriy <mrdikukushkin@yandex.ru>
2021-09-20 21:03:29 +0300

    можно задавать название файла для лога

--- decorator.py
+++ decorator.py
@@ -10,9 +10,10 @@ def decorator(old_func):
         time = str(datetime.datetime.now())
         result = old_func(*args, **kwargs)
         log_dict = {'Time_func': time, 'Name_func': name_func, 'Arg_func': f"{args}, {kwargs}", 'Result': result}
-        with open('logs.json', 'a', encoding='utf-8') as f:
+        logs_f = input('Введите название файла для сохранения логов:')
+        with open(f'{logs_f}{".json"}', 'a', encoding='utf-8') as f:
             json.dump(log_dict, f, ensure_ascii=False, indent=4)
-        log = os.path.abspath('logs.json')
-        print(f'Файлы расположены: {log}')
+        log = os.path.abspath(logs_f)
+        print(f'Файлы расположены: {logs_f}{".json"}')
         return result
     return new_func
